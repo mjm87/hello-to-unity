d40d73bb907800707c5e905830953844ba5a3a21
Michel Momeyer <mjm87@calvin.edu>
2018-10-05 09:01:34 -0400

    Created a default card setup

---
+++ Assets/ScriptableObject Code/CardData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CardData", menuName = "Create New Card")]
+public class CardData : ScriptableObject
+{
+    public Sprite portrait;
+    public float conversion_power;
+    public float faith;
+    public string name;
+	
+    // private float currentFaith;
+}
---
+++ Assets/Scripts/CardScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour {
+
+    public CardData data;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SpriteRenderer>().sprite = data.portrait;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
