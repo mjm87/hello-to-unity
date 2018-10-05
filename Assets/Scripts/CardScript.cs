5b9f867580871cc357307e72edbad30f9b73cb47
MichelMomeyer <strihawk1213@gmail.com>
2018-10-05 16:31:39 -0400

    Refactored data to be more descriptive

--- Assets/Scripts/CardScript.cs
+++ Assets/Scripts/CardScript.cs
@@ -5,13 +5,14 @@ using TMPro;
 
 public class CardScript : MonoBehaviour {
 
-    public CardData data;
+    public CardData portrait_data;
 
 	// Use this for initialization
 	void Start () {
-        transform.Find("Portrait").GetComponent<SpriteRenderer>().sprite = data.portrait;
-        transform.Find("Card Canvas/Conversion Power").GetComponent<TextMeshProUGUI>().text = "" + data.conversion_power;
-        transform.Find("Card Canvas/Faith").GetComponent<TextMeshProUGUI>().text = "" + data.faith;
+        // initializing card with portrait_portrait_data
+        transform.Find("Portrait").GetComponent<SpriteRenderer>().sprite = portrait_data.portrait;
+        transform.Find("Card Canvas/Conversion Power").GetComponent<TextMeshProUGUI>().text = "" + portrait_data.conversion_power;
+        transform.Find("Card Canvas/Faith").GetComponent<TextMeshProUGUI>().text = "" + portrait_data.faith;
 	}
 	
 	// Update is called once per frame
