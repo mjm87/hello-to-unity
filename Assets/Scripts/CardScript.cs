22fc42ef44863b5c3bae8f3171ebd78cd1cec0ec
MichelMomeyer <strihawk1213@gmail.com>
2018-10-05 16:38:22 -0400

    Save references to the card display components

--- Assets/Scripts/CardScript.cs
+++ Assets/Scripts/CardScript.cs
@@ -7,12 +7,24 @@ public class CardScript : MonoBehaviour {
 
     public CardData portrait_data;
 
-	// Use this for initialization
+
+    // card display components
+    private SpriteRenderer portraitRenderer;
+    private TextMeshProUGUI conversionPowerText;
+    private TextMeshProUGUI faithText;
+
+
 	void Start () {
+
+        // finding the portrait related child components
+        portraitRenderer = transform.Find("Portrait").GetComponent<SpriteRenderer>();
+        conversionPowerText = transform.Find("Card Canvas/Conversion Power").GetComponent<TextMeshProUGUI>();
+        faithText = transform.Find("Card Canvas/Faith").GetComponent<TextMeshProUGUI>();
+
         // initializing card with portrait_portrait_data
-        transform.Find("Portrait").GetComponent<SpriteRenderer>().sprite = portrait_data.portrait;
-        transform.Find("Card Canvas/Conversion Power").GetComponent<TextMeshProUGUI>().text = "" + portrait_data.conversion_power;
-        transform.Find("Card Canvas/Faith").GetComponent<TextMeshProUGUI>().text = "" + portrait_data.faith;
+        portraitRenderer.sprite = portrait_data.portrait;
+        conversionPowerText.text = "" + portrait_data.conversion_power;
+        faithText.text = "" + portrait_data.faith;
 	}
 	
 	// Update is called once per frame
